69bbbe3ccca712ed150c17972a977caeb8c16dd8
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2020-11-04 23:29:51 +0100

    Add pymongo and django-progressbarupload to the requirements.txt. Add all extra packages into the INSTALLED_APPS setting.

--- ner_django_app/settings.py
+++ ner_django_app/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ner_django.apps.NerDjangoConfig',
+    'spacy',
+    'spacy_lookups_data',
+    'pymongo',
+    'progressbarupload'
 ]
 
 MIDDLEWARE = [
