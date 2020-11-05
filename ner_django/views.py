1be22641c656422e950aa1a2fa4f28e1f50d751f
vbukhonov <vladimir.bukhonov@ontarget-group.com>
2020-11-05 03:06:39 +0100

    Use util functions in the views.py.

--- ner_django/views.py
+++ ner_django/views.py
@@ -1,5 +1,14 @@
+import os
+
+from django.conf import settings
+from pymongo import MongoClient
+
 from django.http import HttpResponse, HttpResponseServerError
 from django.shortcuts import render
+
+from ner_django.utils import process_archive
+
+mongodb = MongoClient(f"{settings.MONGO_DB_URI}/{settings.MONGO_DB_NAME}").get_database()
 
 
 def index(request):
@@ -7,8 +16,17 @@ def index(request):
 
 
 def process_files(request):
-    return HttpResponse("Success!")
+    inputs_dir = "./inputs"
+    try:
+        for name in os.listdir(inputs_dir):
+            path = os.path.join(inputs_dir, name)
+            if os.path.isfile(path):
+                process_archive(path, mongodb)
+        return HttpResponse("Success!")
+    except Exception as e:
+        return HttpResponseServerError("Something went wrong")
 
 
 def clean_mongo_database(request):
+    mongodb.patents.drop()
     return HttpResponse("MongoDB is cleaned.")
